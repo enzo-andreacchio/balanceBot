2833cdf51387ee0035b0b597222e9f953fb9e77f
enzo-andreacchio <enzo.andreacchio.perso@gmail.com>
2025-05-06 19:59:41 -0700

    added the balance_Bot folder

---
+++ project_starter/balance_bot/controller.cpp
@@ -0,0 +1,165 @@
+/**
+ * @file controller.cpp
+ * @brief Controller file
+ * 
+ */
+
+#include <SaiModel.h>
+#include "SaiPrimitives.h"
+#include "redis/RedisClient.h"
+#include "timer/LoopTimer.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace Eigen;
+using namespace SaiPrimitives;
+
+#include <signal.h>
+bool runloop = false;
+void sighandler(int){runloop = false;}
+
+#include "redis_keys.h"
+
+// States 
+enum State {
+	IDLE = 0,
+	BALANCE
+};
+
+int main() {
+	// Location of URDF files specifying world and robot information
+	static const string robot_file = string(CS225A_URDF_FOLDER) + "/panda/panda_arm_balance_bot.urdf";
+
+	// initial state 
+	int state = IDLE;
+	string controller_status = "1";
+	
+	// start redis client
+	auto redis_client = SaiCommon::RedisClient();
+	redis_client.connect();
+
+	// set up signal handler
+	signal(SIGABRT, &sighandler);
+	signal(SIGTERM, &sighandler);
+	signal(SIGINT, &sighandler);
+
+	// load robots, read current state and update the model
+	auto robot = std::make_shared<SaiModel::SaiModel>(robot_file, false);
+	robot->setQ(redis_client.getEigen(JOINT_ANGLES_KEY));
+	robot->setDq(redis_client.getEigen(JOINT_VELOCITIES_KEY));
+	robot->updateModel();
+
+	// prepare controller
+	int dof = robot->dof();
+	VectorXd command_torques = VectorXd::Zero(dof);  // panda + gripper torques 
+	MatrixXd N_prec = MatrixXd::Identity(dof, dof);
+
+	// arm task
+	const string control_link = "link7";
+	const Vector3d control_point = Vector3d(0, 0, 0.17);
+	Affine3d compliant_frame = Affine3d::Identity();
+	compliant_frame.translation() = control_point;
+	auto pose_task = std::make_shared<SaiPrimitives::MotionForceTask>(robot, control_link, compliant_frame);
+	pose_task->setPosControlGains(400, 40, 0);
+	pose_task->setOriControlGains(400, 40, 0);
+
+	Vector3d ee_pos;
+	Matrix3d ee_ori;
+
+
+	// joint task
+	auto joint_task = std::make_shared<SaiPrimitives::JointTask>(robot);
+	joint_task->setGains(400, 40, 0);
+
+	VectorXd q_desired(dof);
+	q_desired << -73.05, -57.93, 64.52, -144.73, -128.29, 77.12, 40.70;
+	q_desired *= M_PI / 180.0;
+	joint_task->setGoalPosition(q_desired);
+
+
+	VectorXd ee_pos_desired(3); 
+
+
+
+	// create a loop timer
+	runloop = true;
+	double control_freq = 1000;
+	SaiCommon::LoopTimer timer(control_freq, 1e6);
+
+	while (runloop) {
+		timer.waitForNextLoop();
+		const double time = timer.elapsedSimTime();
+
+		// update robot 
+		robot->setQ(redis_client.getEigen(JOINT_ANGLES_KEY));
+		robot->setDq(redis_client.getEigen(JOINT_VELOCITIES_KEY));
+		robot->updateModel();
+	
+		if (state == IDLE) {
+			// update task model 
+			N_prec.setIdentity();
+			joint_task->updateTaskModel(N_prec);
+
+			command_torques = joint_task->computeTorques();
+
+
+			Vector3d force = redis_client.getEigen(FORCE_SENSOR_KEY);
+			// cout << "force local frame:\t" << force.transpose() << endl;
+
+			float Fz = force(2);
+
+			float Fz_thr = 0.1;
+
+			Vector3d moment = redis_client.getEigen(MOMENT_SENSOR_KEY);
+			cout << "moment local frame:\t" << moment.transpose() << endl;
+
+			if (abs(Fz) > Fz_thr) {
+				cout << "Idle to Balance" << endl;
+
+				pose_task->reInitializeTask();
+				joint_task->reInitializeTask();
+
+				ee_pos = robot->position(control_link, control_point);
+				ee_ori = robot->rotation(control_link);
+
+				pose_task->setGoalPosition(ee_pos + Vector3d(-0.1, -0.1, 0.1));
+				pose_task->setGoalOrientation(AngleAxisd(M_PI / 6, Vector3d::UnitX()).toRotationMatrix() * ee_ori);
+
+				// state = BALANCE;
+			}
+
+
+			
+
+
+
+		} else if (state == BALANCE) {
+
+			
+
+			// update goal position and orientation
+			ee_pos_desired << 0.5, 0.5, 0.5+0.3*sin(time);
+			pose_task->setGoalPosition(ee_pos_desired);
+
+
+			// update task model
+			N_prec.setIdentity();
+			pose_task->updateTaskModel(N_prec);
+			joint_task->updateTaskModel(pose_task->getTaskAndPreviousNullspace());
+
+			command_torques = pose_task->computeTorques() + joint_task->computeTorques();
+		}
+
+		// execute redis write callback
+		redis_client.setEigen(JOINT_TORQUES_COMMANDED_KEY, command_torques);
+	}
+
+	timer.stop();
+	cout << "\nSimulation loop timer stats:\n";
+	timer.printInfoPostRun();
+	redis_client.setEigen(JOINT_TORQUES_COMMANDED_KEY, 0 * command_torques);  // back to floating
+
+	return 0;
+}
